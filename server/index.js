dcfe0918b0cdb5c9c52f5ca6e56f35a1da6d296d
Himan Khorshidi <himan.khorshidi@hs.stud-ruhrwest.de>
2025-05-20 22:17:58 +0200

    Frontend: API-Anbindung getestet (GET /api/test)

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const PORT = 3000;
 app.get('/', (req, res) => {
   res.send('Server läuft!');
 });
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'API-Verbindung erfolgreich' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
